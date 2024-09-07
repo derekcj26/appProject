6b2229f91fa2735317c9b3f2acda67c54751b211
Derek Cerda - Jimenez <110270223+derekcj26@users.noreply.github.com>
2024-09-07 19:50:37 +1200

    First Skeleton

---
+++ dev/BA/app/(tabs)/home.jsx
@@ -0,0 +1,18 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Home = () => {
+  return (
+    <SafeAreaView className ="bg-primary h-full">
+      <ScrollView>
+
+
+
+      </ScrollView>
+
+    </SafeAreaView>
+  )
+}
+
+export default Home
